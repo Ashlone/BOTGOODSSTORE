b6dc9f0cbb61202c18862a3794e49fba02ab2500
Ashlone <ashchibvuri@gmail.com>
2021-04-22 08:33:03 +0200

    payfast changes

--- src/views/checkout/step3/Total.jsx
+++ src/views/checkout/step3/Total.jsx
@@ -1,4 +1,5 @@
 import { ArrowLeftOutlined, CheckOutlined } from '@ant-design/icons';
+import { BasketItem } from 'components/basket';
 import { CHECKOUT_STEP_2 } from 'constants/routes';
 import { useFormikContext } from 'formik';
 import { displayMoney } from 'helpers/utils';
@@ -19,7 +20,7 @@ const Total = ({ subtotal }) => {
   const PayfastPayment = () => {
     let PayfastConfig = {
       'amount': subtotal,
-      'item_name':  <BasketItem/>,
+      'item_name':  BasketItem,
       'return_url': `https://master.d14qrahcewz8qq.amplifyapp.com/`,
       'cancel_url': `https://master.d14qrahcewz8qq.amplifyapp.com/`,
       'return_dev': 'https://sandbox.payfast.co.za/eng/process?merchant_id=10015422&merchant_key=ov9u3i696izit',
